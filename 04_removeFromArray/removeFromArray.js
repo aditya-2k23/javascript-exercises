af399f1d14ebefe135482be80709c4d4170eb52c
Aditya <holaaditya123@gmail.com>
2024-07-27 17:53:32 +0530

    Passed!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+  let args = Array.from(arguments);
+  args.shift();
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!args.includes(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line

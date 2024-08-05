bb0ab33172d3cdc16a1126decff9720c8a3cc099
Aditya <holaaditya123@gmail.com>
2024-08-05 23:28:22 +0530

    Passed!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let reg = /[\W_]/g;
+  let lowRegStr = str.toLowerCase().replace(reg, '');
+  let reverseStr = lowRegStr.split('').reverse().join('');
+  return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line

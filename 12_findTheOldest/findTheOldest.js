88d485d2ba50fe37461bdaba04f4c7fcdc7c58b6
Aditya <holaaditya123@gmail.com>
2024-08-05 23:33:02 +0530

    Passed!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest);
+    const currentAge = getAge(current);
+    return oldestAge < currentAge ? current : oldest;
+  });
+};
 
+const getAge = function (person) {
+  return person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth;
 };
 
 // Do not edit below this line
